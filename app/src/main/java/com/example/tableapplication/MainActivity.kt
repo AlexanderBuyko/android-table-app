1e8d6fcc37c2223323d89d951f82ee73cdc48846
Alexander Buyko <>
2022-09-13 19:54:51 +0300

    Add fix for splash screen performance

--- app/src/main/java/com/example/tableapplication/MainActivity.kt
+++ app/src/main/java/com/example/tableapplication/MainActivity.kt
@@ -15,6 +15,9 @@ import androidx.core.animation.doOnStart
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.*
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var componentContainer: LinearLayout
     private lateinit var splashIcon: ImageView
     private lateinit var bullet: ImageView
+
+    private var exitAnimationPreparation: Deferred<Unit>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,10 +47,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareForSplashExitAnimation() {
-        lifecycleScope.launchWhenResumed {
+        exitAnimationPreparation = lifecycleScope.async {
             val bitmap = splashIcon.drawToBitmap()
             val leftBitmapPart = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width / 2, bitmap.height)
-            val rightBitmapPart = Bitmap.createBitmap(bitmap, bitmap.width / 2, 0, bitmap.width / 2, bitmap.height)
+            val rightBitmapPart =
+                Bitmap.createBitmap(bitmap, bitmap.width / 2, 0, bitmap.width / 2, bitmap.height)
             curtainLeft.setImageBitmap(leftBitmapPart)
             curtainRight.setImageBitmap(rightBitmapPart)
         }
@@ -92,36 +98,39 @@ class MainActivity : AppCompatActivity() {
     private fun handleSplashExitAnimation() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             splashScreen.setOnExitAnimationListener { splashScreenView ->
-                val slideLeft = ObjectAnimator.ofFloat(curtainLeft, View.TRANSLATION_X, 0f, -curtainLeft.width.toFloat())
-                val alphaLeft = ObjectAnimator.ofFloat(curtainLeft, "alpha", 1f, 0f)
-                val slideRight = ObjectAnimator.ofFloat(curtainRight, View.TRANSLATION_X, 0f, curtainRight.width.toFloat())
-                val alphaRight = ObjectAnimator.ofFloat(curtainRight, "alpha", 1f, 0f)
+                lifecycleScope.launch {
+                    exitAnimationPreparation?.await()
+                    val slideLeft = ObjectAnimator.ofFloat(curtainLeft, View.TRANSLATION_X, 0f, -curtainLeft.width.toFloat())
+                    val alphaLeft = ObjectAnimator.ofFloat(curtainLeft, "alpha", 1f, 0f)
+                    val slideRight = ObjectAnimator.ofFloat(curtainRight, View.TRANSLATION_X, 0f, curtainRight.width.toFloat())
+                    val alphaRight = ObjectAnimator.ofFloat(curtainRight, "alpha", 1f, 0f)
 
-                val curtainsPullingAnim = AnimatorSet().apply {
-                    playTogether(slideLeft, alphaLeft, slideRight, alphaRight)
-                    interpolator = DecelerateInterpolator()
-                    duration = CURTAINS_PULLING_ANIMATION_DURATION
-                    startDelay = BULLET_FLYING_ANIMATION_DURATION / 2
-
-                    doOnStart { splashIcon.visibility = View.GONE }
-                    doOnEnd {
-                        curtainLeft.visibility = View.GONE
-                        curtainRight.visibility = View.GONE
-                    }
-                }
-
-                val bulletFlyingAnim =
-                    ObjectAnimator.ofFloat(bullet, View.TRANSLATION_Y, 0f, componentContainer.height.toFloat()).apply {
+                    val curtainsPullingAnim = AnimatorSet().apply {
+                        playTogether(slideLeft, alphaLeft, slideRight, alphaRight)
                         interpolator = DecelerateInterpolator()
-                        duration = BULLET_FLYING_ANIMATION_DURATION
+                        duration = CURTAINS_PULLING_ANIMATION_DURATION
+                        startDelay = BULLET_FLYING_ANIMATION_DURATION / 2
 
-                        doOnEnd { bullet.visibility = View.VISIBLE }
+                        doOnStart { splashIcon.visibility = View.GONE }
+                        doOnEnd {
+                            curtainLeft.visibility = View.GONE
+                            curtainRight.visibility = View.GONE
+                        }
                     }
 
-                AnimatorSet().apply {
-                    playTogether(bulletFlyingAnim, curtainsPullingAnim)
-                    doOnStart { splashScreenView.remove() }
-                    start()
+                    val bulletFlyingAnim =
+                        ObjectAnimator.ofFloat(bullet, View.TRANSLATION_Y, 0f, componentContainer.height.toFloat()).apply {
+                            interpolator = DecelerateInterpolator()
+                            duration = BULLET_FLYING_ANIMATION_DURATION
+
+                            doOnEnd { bullet.visibility = View.VISIBLE }
+                        }
+
+                    AnimatorSet().apply {
+                        playTogether(bulletFlyingAnim, curtainsPullingAnim)
+                        doOnStart { splashScreenView.remove() }
+                        start()
+                    }
                 }
             }
         }
